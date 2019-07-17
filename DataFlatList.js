67e38d5773ffd8fd81260e5cc6a21c1c48038ab8
Prashant Tyagi <prashanttyagi@UCI.guest.corp.microsoft.com>
2019-07-17 15:49:40 +0530

    added net info and axios

--- DataFlatList.js
+++ DataFlatList.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text, FlatList, Image ,ImageBackground } from 'react-native';
+import { View, Text, FlatList, Image, ImageBackground } from 'react-native';
 
+import NetInfo from "@react-native-community/netinfo";
+import Toast, {DURATION} from 'react-native-easy-toast'
 
 class DataFlatList extends React.Component {
 
@@ -10,18 +12,36 @@ class DataFlatList extends React.Component {
         this.state = { testdata: [1, 2, 3] };
     }
 
+    componentDidMount() {
+        NetInfo.fetch().then(state => {
+            console.log("Connection type", state.type);
+            console.log("Is connected?", state.isConnected);
+            this.refs.toast.show( state.type+state.isConnected);
+        });
+
+        // Subscribe
+        const unsubscribe = NetInfo.addEventListener(state => {
+            console.log("Connection type", state.type);
+            console.log("Is connected?", state.isConnected);
+        });
+
+        // Unsubscribe
+        //unsubscribe();
+
+
+    }
     _keyExtractor = (item, index) => item.toString();
 
     _renderItem = ({ item }) => (
 
         <View>
-            <ImageBackground 
+            <ImageBackground
                 source={{ uri: 'https://cdn.pixabay.com/photo/2016/11/30/08/48/bedroom-1872196_1280.jpg' }}
-                style={{ width: 150, height: 150, flexDirection:'column', justifyContent:'flex-end' }}
+                style={{ width: 150, height: 150, flexDirection: 'column', justifyContent: 'flex-end' }}
             >
-                <Text style={{backgroundColor:'orange'}}>{item}</Text>
+                <Text style={{ backgroundColor: 'orange' }}>{item}</Text>
             </ImageBackground>
-            
+
         </View>
     );
 
@@ -29,6 +49,7 @@ class DataFlatList extends React.Component {
         return (
             <View>
                 <Text>Hello World</Text>
+                <Toast ref="toast"/>
                 <FlatList
                     data={this.state.testdata}
                     // extraData={this.state}
